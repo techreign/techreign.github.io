7f7e2bf347ef118d9b4acd06013a2d2905c2cdff
Shoaib <shoaibmaster1@gmail.com>
2018-01-07 04:27:13 -0500

    Date and Time page completed

---
+++ Javascript/date_teller.js
@@ -0,0 +1,27 @@
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
+}
+
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	var d = today.getDate();
+	var mo = today.getMonth();
+	var y = today.getYear();
+	// adding a zero in front of numbers less than 10
+	m = checkTime(m);
+	s = checkTime(s);
+	y = y + 1900;
+	mo = mo + 1;
+	document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+	document.getElementById('datee').innerHTML = mo + "/" + d + "/" + y;
+	
+	t = setTimeout(function() {startTime()}, 500);
+}
+
+startTime();
