d6017de25f160ae07b241a9dc86802675d272155
Shoaib <shoaibmaster1@gmail.com>
2018-01-09 17:30:20 -0500

    added Digit Squarer page

---
+++ Javascript/squaredigits.js
@@ -0,0 +1,16 @@
+// shows the answer
+function showAnswer() {
+	document.getElementById('answer').innerHTML = squareDigit();
+}
+
+// performs the squaring of each digit
+function squareDigit() {
+	var num = document.getElementById('numero').value;
+	var newNum = "";
+	while (num != 0) {
+		var remainder = num % 10;
+		var leftNum = Floor.math(num / 10); 
+		newNum = remainder * remainder + newNum;
+	}
+	return newNum;
+}
